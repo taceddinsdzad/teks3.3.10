9e6089b472b2ff2edb8372eb6ce2c229783093bb
Tacəddin Əsədzadə <tcddnesedzade@fmail.com>
2024-10-03 12:22:34 +0400

    task3

---
+++ task3.10.12/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("eded daxil et");
+int num = Convert.ToInt32(Console.ReadLine());
+int num2 = 0;
+for (int i = 2; i <= num; i *= 2)
+{
+    if (i == num)
+    {
+        num2 = num / i;
+    }
+}
+if (num2 != 0)
+{
+    Console.WriteLine("quvvetdir");
+}
+else
+{
+    Console.WriteLine("deyil");
+}
+
